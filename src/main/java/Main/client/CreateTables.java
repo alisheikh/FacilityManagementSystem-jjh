a2f8b045418bca3437dac11bcdd395e1a77e14d6
athornburg <alexthornburg1@gmail.com>
2014-02-07 00:39:10 -0600

    removing target printing all tables

--- src/main/java/Main/client/CreateTables.java
+++ src/main/java/Main/client/CreateTables.java
@@ -1,7 +1,7 @@
 package Main.client;
 
 import Main.DAL.DatabaseConnector;
-
+import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -14,7 +14,7 @@ public class CreateTables {
 
     public static void main(String[] args){
         Connection connection = DatabaseConnector.connect();
-        try {
+        /*try {
             //connection.createStatement().executeQuery("DROP TABLE IF EXISTS Facility;");
             connection.createStatement().executeQuery("CREATE TABLE Facility (" +
                     "id varchar (25) PRIMARY KEY," +
@@ -22,6 +22,18 @@ public class CreateTables {
                     ")");
         } catch (SQLException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }*/
+        printTables(connection);
+    }
+    public static void printTables(Connection connection){
+        try {
+               ResultSet rs =connection.createStatement().executeQuery("SELECT * FROM information_schema.tables ");
+               while (rs.next()) {
+                    System.out.println(rs.getString(3)+"\t"+rs.getString(4));
+               }
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+
         }
     }
 }
