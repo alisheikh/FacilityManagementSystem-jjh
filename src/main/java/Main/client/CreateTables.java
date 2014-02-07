5540f6bcb5ebcea6e675ac9a22175a0986903256
athornburg <alexthornburg1@gmail.com>
2014-02-07 00:23:47 -0600

    Client Returns all tables.

--- src/main/java/Main/client/CreateTables.java
+++ src/main/java/Main/client/CreateTables.java
@@ -4,6 +4,7 @@ import Main.DAL.DatabaseConnector;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 
 /**
  * User: alexthornburg
@@ -14,12 +15,24 @@ public class CreateTables {
 
     public static void main(String[] args){
         Connection connection = DatabaseConnector.connect();
-        try {
-            //connection.createStatement().executeQuery("DROP TABLE IF EXISTS Facility;");
+        /*try {
             connection.createStatement().executeQuery("CREATE TABLE Facility (" +
                     "id varchar (25) PRIMARY KEY," +
                     "buildingNumber int NOT NULL" +
                     ")");
+
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }*/
+        printTables(connection);
+    }
+
+    public static void printTables(Connection connection){
+        try {
+            ResultSet rs =connection.createStatement().executeQuery("SELECT * FROM information_schema.tables ");
+            while (rs.next()) {
+                System.out.println(rs.getString(3)+"\t"+rs.getString(4));
+            }
         } catch (SQLException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
