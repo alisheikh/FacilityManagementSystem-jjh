9126c59b6950ad0613765f75cda7e3a5be50584b
jacksonjhayes <jacksonjhayes@gmail.com>
2014-03-16 16:16:02 -0500

    Made a couple of changes Hibernate not working
    Yet

---
+++ src/main/java/Main/DAL/UnitDAOHib.java
@@ -0,0 +1,93 @@
+package Main.DAL;
+
+import Main.Entities.Facility.Unit;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Created by Jackson on 3/16/14.
+ */
+public class UnitDAOHib implements IUnitDAO  {
+    private IDatabaseConnector connector;
+    private Connection connection;
+
+    @Override
+    public void CreateUnit(List<Unit> units) throws SQLException {
+        for(Unit unit:units){
+            CreateUnit(unit);
+        }
+
+    }
+
+    @Override
+    public Unit CreateUnit(Unit unit) throws SQLException {
+        Session session = HibernatePGSQLHelper.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        session.save(unit);
+        session.getTransaction().commit();
+
+        return unit;
+    }
+
+    @Override
+    public Unit GetUnit(int unit_id) throws SQLException {
+        try {
+            System.out.println("*************** Searcing for unit information with ID ...  " + unit_id);
+            Session session = HibernatePGSQLHelper.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+
+            //System.out.println("*************** Hibernate session is created ..................\n" + session.toString());
+
+            //Query getCustQuery = session.createQuery("From CustomerImpl ");
+            Query getCustQuery = session.createQuery("From CustomerImpl where unitId=:unit_id");
+            getCustQuery.setInteger("unit_id", unit_id);
+
+            System.out.println("*************** Retrieve Query is ....>>\n" + getCustQuery.toString());
+
+            List units = getCustQuery.list();
+            //System.out.println("Getting Book Details using HQL. \n" + units);
+
+            session.getTransaction().commit();
+            return (Unit)units.get(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public Unit UpdateUnit(Unit unit) throws Exception {
+        return null;
+    }
+
+    @Override
+    public List<Unit> GetUnitForFacility(int id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void DeleteUnit(int id) {
+
+    }
+
+    public IDatabaseConnector getConnector() {
+        return connector;
+    }
+
+    public void setConnector(IDatabaseConnector connector) {
+        this.connector = connector;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+}
